85e3a2631a54a44b514bedc202c755486a0c20af
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2023-09-11 21:33:23 +0200

    Add smoketest

--- tests/test_fucciphase.py
+++ tests/test_fucciphase.py
@@ -1,5 +1,17 @@
-def test_smoke_pipeline():
-    """Test that the package can be imported."""
-    import fucciphase
+from fucciphase.phase import compute_phase_trigo
+from fucciphase.simulator import simulate_single_track
+from fucciphase.utils import normalize_channels
 
-    assert fucciphase.__version__ is not None
+
+def test_smoke_pipeline():
+    """Test that the pipeline can run on simulated data."""
+    # simulate a single track
+    df = simulate_single_track()
+
+    # normalize the channels
+    channel1 = "MEAN_INTENSITY_CH3"
+    channel2 = "MEAN_INTENSITY_CH4"
+    normalize_channels(df, [channel1, channel2])
+
+    # compute the phase
+    compute_phase_trigo(df, channel1, channel2)
