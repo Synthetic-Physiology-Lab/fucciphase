Add track IDs, add moving average, improve simulator, fix tests

src/fucciphase/utils/normalize.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import List, Optional, Union
 
 import numpy as np
 import pandas as pd
@@ -18,6 +18,22 @@ def get_norm_channel_name(channel: str) -> str:
         Name of the normalized channel.
     """
     return f"{channel}_NORM"
+
+
+def get_avg_channel_name(channel: str) -> str:
+    """Return the name of the moving-averaged channel.
+
+    Parameters
+    ----------
+    channel : str
+        Name of the channel to average using moving average.
+
+    Returns
+    -------
+    str
+        Name of the moving-averaged channel.
+    """
+    return f"{channel}_AVG"
 
 
 def norm(vector: Union[pd.Series, np.ndarray]) -> Union[pd.Series, np.ndarray]:
@@ -43,12 +59,92 @@ def norm(vector: Union[pd.Series, np.ndarray]) -> Union[pd.Series, np.ndarray]:
     return norm_ch
 
 
-def normalize_channels(df: pd.DataFrame, channels: Union[str, List[str]]) -> List[str]:
+# TODO: is there a simpler way? The convolution is probably an advantage for large v
+def moving_average(vector: Union[pd.Series, np.ndarray], window: int = 7) -> np.ndarray:
+    """Compute the moving average of a vector using a fixed window.
+
+    The returned vector has the same size as the input vector. The moving average
+    function breaks the problem in two parts:
+        - Edges: computed using a an increasing window size (starting from w//2)
+        - Middle: computed using a fixed window size (w)
+
+    The window size must be odd.
+
+    Parameters
+    ----------
+    vector : Union[pd.Series, np.ndarray]
+        Vector to average.
+    window : int
+        Size of the window, must be odd.
+
+    Returns
+    -------
+    Union[pd.Series, np.ndarray]
+        Averaged vector.
+    """
+    if window < 1:
+        raise ValueError(f"Window size must be > 0, got {window}")
+
+    if window % 2 == 0:
+        raise ValueError(f"Window size must be odd, got {window}")
+
+    # if the window is larger than the vector, return the vector
+    if window >= len(vector):
+        return vector
+
+    # compute the cumulative sum
+    cumsum = np.cumsum(vector)
+
+    # compute left-hand edge using the cumulative sum
+    # index i starts at 0, and ends when the window is one index away from fitting on
+    # the left-hand side
+    # cumsum is the cumulative sum of the whole vector
+    # the window size increases with the indices
+    pre_vector = np.array([cumsum[i] / (i + 1) for i in range(window // 2, window - 1)])
+
+    # compute right-hand edge using the cumulative sum
+    # index i starts when the window does not fit anymore in the right-hand side
+    # therefore i starts at len(vector) - window//2, and ends at the last index
+    # cumsum[-1] is the sum of the whole vector
+    # cumsum[i - window//2 - 1] is the sum until the index before the window starts
+    # the difference between the two is the sum of the window
+    # we divide by the number of elements in the window, the window reduces in size
+    # for each index
+    post_vector = np.array(
+        [
+            (cumsum[-1] - cumsum[i - window // 2 - 1]) / (window // 2 + len(vector) - i)
+            for i in range(len(vector) - window // 2, len(vector))
+        ]
+    )
+
+    # compute valid convolution for the middle part
+    middle_vector = np.array(vector, dtype=float)
+    middle_vector = np.convolve(middle_vector, np.ones(window), mode="valid") / window
+
+    return np.concatenate([pre_vector, middle_vector, post_vector], dtype=float)
+
+
+def normalize_channels(
+    df: pd.DataFrame,
+    channels: Union[str, List[str]],
+    use_moving_average: bool = False,
+    moving_average_window: int = 7,
+    manual_min: Optional[float] = None,
+    manual_max: Optional[float] = None,
+) -> List[str]:
     """Normalize channels, add in place the resulting columns to the
     dataframe, and return the new columns' name.
 
+    A moving average can be applied to each individual track before normalization.
+
     Normalization is performed by subtracting the min and dividing by (max - min).
     Note that the resulting values are rounded to the 2nd decimal.
+
+    The min and max values can be provided manually. They should be determined by
+    imaging a large number of cells statically and computing the min and max values
+    observed.
+
+    TODO: add not about ergodicity, more details about static imaging
 
     Parameters
     ----------
@@ -56,6 +152,14 @@ def normalize_channels(df: pd.DataFrame, channels: Union[str, List[str]]) -> Lis
         Dataframe
     channels : Union[str, List[str]]
         Name of the channels to normalize.
+    use_moving_average : bool
+        Whether to apply a moving average to each track before normalization.
+    moving_average_window : int
+        Size of the window used for the moving average, default 7.
+    manual_min : Optional[float]
+        If provided, the minimum value to use for normalization.
+    manual_max : Optional[float]
+        If provided, the maximum value to use for normalization.
 
     Returns
     -------
@@ -76,11 +180,32 @@ def normalize_channels(df: pd.DataFrame, channels: Union[str, List[str]]) -> Lis
         if channel not in df.columns:
             raise ValueError(f"Column {channel} not found")
 
+        # apply moving average to each track ID
+        unique_track_IDs = df["TRACK_ID"].unique()
+
+        # compute the moving average for each track ID
+        if use_moving_average:
+            avg_channel = get_avg_channel_name(channel)
+            for track_ID in unique_track_IDs:
+                # get the track
+                track: pd.DataFrame = df[df["TRACK_ID"] == track_ID]
+
+                # sort the channel by frame
+                track = track.sort_values(by="FRAME")
+
+                # compute the moving average
+                ma = moving_average(track[channel], window=moving_average_window)
+
+                # update the dataframe by adding a new column
+                df.loc[track.index, avg_channel] = ma
+        else:
+            avg_channel = channel
+
         # normalize channel
-        max_ch = df[channel].max()
-        min_ch = df[channel].min()
+        max_ch = df[avg_channel].max()
+        min_ch = df[avg_channel].min()
         norm_ch = np.round(
-            (df[channel] - min_ch) / (max_ch - min_ch),
+            (df[avg_channel] - min_ch) / (max_ch - min_ch),
             2,  # number of decimals
         )
 

src/fucciphase/utils/simulator.py
@@ -27,11 +27,18 @@ def simulate_single_channel(
     """
     ch: np.ndarray = amp * np.exp(-((t - mean) ** 2) / (2 * sigma**2))
 
-    return ch
+    return np.round(ch, 2)
 
 
-def simulate_single_track() -> pd.DataFrame:
+def simulate_single_track(track_id: float = 42, mean: float = 0.5) -> pd.DataFrame:
     """Simulate a single track.
+
+    Parameters
+    ----------
+    track_id : int
+        Track ID
+    mean : float
+        Temporal mean corresponding to the crossing between the two channels
 
     Returns
     -------
@@ -41,10 +48,10 @@ def simulate_single_track() -> pd.DataFrame:
     # create the time vector
     t = 24 * np.arange(0, 50) / 50
     sigma = 0.2 * 24
-    mean1 = 0.5 * 24 - sigma
-    mean2 = 0.5 * 24 + sigma
-    amp1 = 1
-    amp2 = 0.9
+    mean1 = mean * 24 - sigma
+    mean2 = mean * 24 + sigma
+    amp1 = 50
+    amp2 = 0.9 * 50
 
     # create the channels as Gaussian of time
     ch1 = simulate_single_channel(t, mean1, sigma, amp1)
@@ -54,10 +61,11 @@ def simulate_single_track() -> pd.DataFrame:
     df = pd.DataFrame(
         {
             "LABEL": [f"ID{i}" for i in range(len(t))],
-            "ID": [str(i) for i in range(len(t))],
-            "TRACK_ID": [85 for _ in range(len(t))],
-            "POSITION_X": [105.567 for _ in range(len(t))],
-            "POSITION_Y": [82.23 for _ in range(len(t))],
+            "ID": list(range(len(t))),
+            "TRACK_ID": [track_id for _ in range(len(t))],
+            "POSITION_X": [np.round(mean * i * 0.02, 2) for i in range(len(t))],
+            "POSITION_Y": [np.round(mean * i * 0.3, 2) for i in range(len(t))],
+            "POSITION_T": [np.round(i * 0.01, 2) for i in range(len(t))],
             "FRAME": list(range(len(t))),
             "MEAN_INTENSITY_CH1": [0 for _ in range(len(t))],
             "MEAN_INTENSITY_CH2": [1 for _ in range(len(t))],

src/fucciphase/utils/trackmate.py
@@ -2,6 +2,7 @@ import xml.etree.ElementTree as et
 from pathlib import Path
 from typing import Dict, List, Optional, Union
 
+import numpy as np
 import pandas as pd
 
 # TrackMate XML tags
@@ -61,9 +62,9 @@ class TrackMateXML:
         self._model: Optional[et.Element] = None
         self._allspots: Optional[et.Element] = None
 
-        self.nspots: int = 0
-        self.features: Dict[str, type] = {}
-        self.spot_features: List[str] = []
+        self.nspots: int = 0  # number of spots
+        self.features: Dict[str, type] = {}  # features and their types
+        self.spot_features: List[str] = []  # list of spot features
 
         # import model and all spots
         self._import_data()
@@ -138,6 +139,36 @@ class TrackMateXML:
         # get feature declarations
         self._get_features()
 
+    def _add_track_ids(self, df: pd.DataFrame) -> None:
+        # extract track IDs (if there are spots)
+        if len(df) > 0:
+            track_ids = np.zeros_like(df[ID].values)
+            if self._model is not None:
+                for element in self._model:
+                    if element.tag == "AllTracks":
+                        for track in element:
+                            track_id = track.attrib["TRACK_ID"]
+
+                            for edge in track:
+                                spot_source = edge.attrib["SPOT_SOURCE_ID"]
+                                spot_target = edge.attrib["SPOT_TARGET_ID"]
+
+                                # get row index of the source and target spots
+                                source_index = df[df[ID] == spot_source].index[0]
+                                target_index = df[df[ID] == spot_target].index[0]
+
+                                # update track IDs
+                                track_ids[source_index] = track_id
+                                track_ids[target_index] = track_id
+
+                # update dataframe
+                df["TRACK_ID"] = track_ids
+        else:
+            df["TRACK_ID"] = []
+
+        # add track id to the spot features (this avoids exporting it to the xml later)
+        self.spot_features.append("TRACK_ID")
+
     def to_pandas(self) -> pd.DataFrame:
         """Export the spots as a pandas dataframe.
 
@@ -163,6 +194,9 @@ class TrackMateXML:
                         # add the spot to the dataframe
                         df.loc[spot_count] = spot.attrib
                         spot_count += 1
+
+        # add tracks IDs
+        self._add_track_ids(df)
 
         # convert features to their declared types
         return df.astype(self.features)

tests/test_normalize.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-from fucciphase.utils import norm, normalize_channels
+import pytest
+from fucciphase.utils import moving_average, norm, normalize_channels
 
 
 def test_norm():
@@ -23,26 +24,47 @@ def test_norm():
     assert norm_df.equals(expected_df["vector"])
 
 
-def test_normalize(trackmate_df: pd.DataFrame):
+@pytest.mark.parametrize("use_ma", [True, False])
+def test_normalize(trackmate_df: pd.DataFrame, use_ma):
     """Normalize the channels and test that the columns have
     been added to the dataframe."""
 
     # normalize the channels
-    channel1 = "MEAN_INTENSITY_CH1"
-    channel2 = "MEAN_INTENSITY_CH2"
-    new_channels = normalize_channels(trackmate_df, [channel1, channel2])
+    channel1 = "MEAN_INTENSITY_CH3"
+    channel2 = "MEAN_INTENSITY_CH4"
+    new_channels = normalize_channels(
+        trackmate_df, [channel1, channel2], use_moving_average=use_ma
+    )
 
     # check that the columns have been added
-    channel1_norm = "MEAN_INTENSITY_CH1_NORM"
-    channel2_norm = "MEAN_INTENSITY_CH2_NORM"
+    channel1_norm = "MEAN_INTENSITY_CH3_NORM"
+    channel2_norm = "MEAN_INTENSITY_CH4_NORM"
     assert channel1_norm in trackmate_df.columns and channel1_norm in new_channels
     assert channel2_norm in trackmate_df.columns and channel2_norm in new_channels
 
-    # check that the values are correct
-    ch1 = trackmate_df[channel1_norm]
-    norm_ch1 = (ch1 - ch1.min()) / (ch1.max() - ch1.min())
-    assert (norm_ch1 == trackmate_df[channel1_norm]).all()
+    # check if normalized
+    assert trackmate_df[channel1_norm].min() == 0
+    assert trackmate_df[channel1_norm].max() == 1
+    assert trackmate_df[channel2_norm].min() == 0
+    assert trackmate_df[channel2_norm].max() == 1
 
-    ch2 = trackmate_df[channel2_norm]
-    norm_ch2 = (ch2 - ch2.min()) / (ch2.max() - ch2.min())
-    assert (norm_ch2 == trackmate_df[channel2_norm]).all()
+
+@pytest.mark.parametrize(
+    "window, expected",
+    [
+        (3, [0.5, 1, 2, 3, 4, 5, 6, 7, 8, 8.5]),
+        (5, [1, 1.5, 2, 3, 4, 5, 6, 7, 7.5, 8]),
+        (7, [1.5, 2, 2.5, 3, 4, 5, 6, 6.5, 7, 7.5]),
+    ],
+)
+def test_moving_average(window, expected):
+    """Test that the moving average function works as expected."""
+    v = np.arange(0, 10)
+
+    # expected value as a np array
+    expected = np.array(expected)
+
+    # run moving average
+    result = moving_average(v, window)
+    assert result.shape == expected.shape
+    assert (result == expected).all()

tests/test_simulator.py
@@ -1,5 +1,5 @@
 import numpy as np
-from fucciphase.simulator import simulate_single_channel, simulate_single_track
+from fucciphase.utils.simulator import simulate_single_channel, simulate_single_track
 
 
 def test_simulate_single_channel():
@@ -18,8 +18,8 @@ def test_simulate_single_channel():
     # check that the min and max are different
     assert ch.min() != ch.max()
 
-    # check that the peak corresponds to the mean position
-    assert ch.argmax() == mean
+    # check that the peak corresponds to about the mean position (rounding errors)
+    assert np.abs(ch.argmax() - mean) <= 1
 
 
 def test_simulate_single_track():