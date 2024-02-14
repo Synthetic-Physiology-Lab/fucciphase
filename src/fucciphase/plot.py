1fd2084ab3896785d21859e04b03698227d934ec
Julius Zimmermann <juliusmanfredkurt.zimmermann@unipv.it>
2024-02-14 19:39:28 +0100

    add new plots

--- src/fucciphase/plot.py
+++ src/fucciphase/plot.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import pandas as pd
 from matplotlib import pyplot as plt
 
@@ -5,8 +7,60 @@ from .phase import NewColumns
 from .utils import get_norm_channel_name
 
 
+def plot_raw_intensities(
+    df: pd.DataFrame,
+    channel1: str,
+    channel2: str,
+    color1: Optional[str] = None,
+    color2: Optional[str] = None,
+    time_column: str = "FRAME",
+    time_label: str = "Frame #",
+) -> None:
+    """TODO description."""
+    ch1_intensity = df[channel1]
+    ch2_intensity = df[channel2]
+
+    t = df[time_column]
+
+    fig, ax1 = plt.subplots()
+
+    # prepare axes
+    ax1.set_xlabel(time_label)
+    ax1.set_ylabel(channel1, color=color1)
+    ax1.tick_params(axis="y", labelcolor=color1)
+    ax2 = ax1.twinx()
+    ax2.set_ylabel(channel2, color=color2)
+    ax2.tick_params(axis="y", labelcolor=color2)
+
+    # plot signal
+    ax1.plot(t, ch1_intensity, color=color1)
+    ax2.plot(t, ch2_intensity, color=color2)
+    fig.tight_layout()
+
+
+def plot_normalized_intensities(
+    df: pd.DataFrame,
+    channel1: str,
+    channel2: str,
+    color1: Optional[str] = None,
+    color2: Optional[str] = None,
+    time_column: str = "FRAME",
+    time_label: str = "Frame #",
+    **plot_kwargs: bool,
+) -> None:
+    """TODO description."""
+    ch1_intensity = df[get_norm_channel_name(channel1)]
+    ch2_intensity = df[get_norm_channel_name(channel2)]
+
+    t = df[time_column]
+    plt.plot(t, ch1_intensity, color=color1, label=channel1, **plot_kwargs)
+    plt.plot(t, ch2_intensity, color=color2, label=channel2, **plot_kwargs)
+    plt.xlabel(time_label)
+    plt.ylabel("Normalised intensity")
+
+
 def plot_phase(df: pd.DataFrame, channel1: str, channel2: str) -> None:
-    """Plot the two channels, the unique intensity and vertical lines
+    """Plot the two channels and vertical lines
     corresponding to the change of phase.
 
     The dataframe must be preprocessed with one of the available phase
