5e19c6a81440cbd6e87f7db8b4474bfc48d3b8cb
Julius Zimmermann <juliusmanfredkurt.zimmermann@unipv.it>
2024-07-11 12:58:15 +0200

    more flexibility for postprocessing

--- src/fucciphase/utils/phase_fit.py
+++ src/fucciphase/utils/phase_fit.py
@@ -14,15 +14,20 @@ def fit_percentages(frames: np.ndarray, percentages: np.ndarray) -> np.ndarray:
     return best_fit
 
 
-def postprocess_estimated_percentages(df: pd.DataFrame) -> None:
+def postprocess_estimated_percentages(df: pd.DataFrame, percentage_column: str) -> None:
     """Make estimated percentages continuous."""
+    if percentage_column not in df:
+        raise ValueError("The name of the percentage column is not in the DataFrame")
     indices = df["TRACK_ID"].unique()
-    df["CELL_CYCLE_PERC_POST"] = np.nan
+    postprocessed_percentage_column = percentage_column + "_POST"
+    df[postprocessed_percentage_column] = np.nan
     for index in indices:
         if index == -1:
             continue
         track = df[df["TRACK_ID"] == index]
         frames = track["FRAME"]
-        percentages = track["CELL_CYCLE_PERC"]
+        percentages = track[percentage_column]
         restored_percentages = fit_percentages(frames, percentages)
-        df.loc[df["TRACK_ID"] == index, "CELL_CYCLE_PERC_POST"] = restored_percentages
+        df.loc[
+            df["TRACK_ID"] == index, postprocessed_percentage_column
+        ] = restored_percentages
